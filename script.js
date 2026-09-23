c3beab91fbb2259dabf4509c292fb22209bb2350
ykanaras <ykanaras@gmail.com>
2026-09-23 10:44:32 -0400

    Add script.js for initial Awakened website

---
+++ script.js
@@ -0,0 +1,1 @@
+const menu=document.querySelector('.menu');const nav=document.querySelector('.nav nav');menu?.addEventListener('click',()=>nav.classList.toggle('open'));document.querySelectorAll('.nav nav a').forEach(a=>a.addEventListener('click',()=>nav.classList.remove('open')));
